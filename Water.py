f137f22a310a02a560dc59d5bfb3ff17bf98a510
DmitriyPudlo <teinalandergor@gmail.com>
2023-01-06 22:16:50 +0500

    It remains to implement the bot

--- Water.py
+++ Water.py
@@ -1,5 +1,5 @@
 import requests
-from Config import token_water, msg_text, dict_wind_dir, dict_condition
+from Config import token_water, msg_text, dict_wind_dir, dict_condition, times_of_day
 
 
 class Water:
@@ -33,17 +33,17 @@ class Water:
         part_two = parts[1]
         part_name_one = part_one['part_name']
         part_name_two = part_two['part_name']
-        forecast_temp_avg_one = part_name_one['temp_avg']
+        forecast_temp_avg_one = part_one['temp_avg']
         forecast_wind_speed_one = part_one['wind_speed']
         forecast_wind_dir_one = part_one['wind_dir']
         forecast_wind_gust_one = part_one['wind_gust']
         forecast_condition_one = part_one['condition']
-        forecast_temp_avg_two = part_name_two['temp_avg']
+        forecast_temp_avg_two = part_two['temp_avg']
         forecast_wind_speed_two = part_two['wind_speed']
         forecast_wind_dir_two = part_two['wind_dir']
         forecast_wind_gust_two = part_two['wind_gust']
         forecast_condition_two = part_two['condition']
         to_bot = msg_text.format(fact_temp, feels_like, dict_wind_dir[wind_dir], wind_speed,  wind_gust, dict_condition[condition],
-                                 part_name_one, forecast_temp_avg_one, dict_wind_dir[forecast_wind_dir_one], forecast_wind_speed_one, forecast_wind_gust_one, dict_condition[forecast_condition_one],
-                                 part_name_two, forecast_temp_avg_two, dict_wind_dir[forecast_wind_dir_two], forecast_wind_speed_two, forecast_wind_gust_two, dict_condition[forecast_condition_two])
+                                 times_of_day[part_name_one], forecast_temp_avg_one, dict_wind_dir[forecast_wind_dir_one], forecast_wind_speed_one, forecast_wind_gust_one, dict_condition[forecast_condition_one],
+                                 times_of_day[part_name_two], forecast_temp_avg_two, dict_wind_dir[forecast_wind_dir_two], forecast_wind_speed_two, forecast_wind_gust_two, dict_condition[forecast_condition_two])
         return to_bot
