abed666803573551d1410eab0aa8e5d366c266a2
DmitriyPudlo <teinalandergor@gmail.com>
2023-01-19 13:24:46 +0500

    Complete (for now)

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 import pass_token
 
-TOKEN_WATER = f'{pass_token.TOKEN_WATER}'
-TOKEN_GEO = f'{pass_token.TOKEN_GEO}'
-TELEGRAM_TOKEN = f'{pass_token.TELEGRAM_TOKEN}'
-TIMEZONE_TOKEN = f'{pass_token.TIMEZONE_TOKEN}'
-DATABASE = f'{pass_token.DATABASE}'
-USER = f'{pass_token.USER}'
-PASSWORD = f'{pass_token.PASSWORD}'
-HOST = f'{pass_token.HOST}'
-USER_MYSQL = f'{pass_token.USER_MYSQL}'
-PASSWORD_MYSQL = f'{pass_token.PASSWORD_MYSQL}'
-DATABASE_MYSQL = f'{pass_token.DATABASE_MYSQL}'
+TOKEN_WATER = input('Weather token')
+TOKEN_GEO = input('Geocode token')
+TELEGRAM_TOKEN = input('Telegram token')
+HOST = input('Host name')
+USER_MYSQL = input('User name')
+PASSWORD_MYSQL = input('Password')
+DATABASE_MYSQL = input('Database name')
+
+# TOKEN_WATER = f'{pass_token.TOKEN_WATER}'
+# TOKEN_GEO = f'{pass_token.TOKEN_GEO}'
+# TELEGRAM_TOKEN = f'{pass_token.TELEGRAM_TOKEN}'
+# TIMEZONE_TOKEN = f'{pass_token.TIMEZONE_TOKEN}'
+# DATABASE = f'{pass_token.DATABASE}'
+# USER = f'{pass_token.USER}'
+# PASSWORD = f'{pass_token.PASSWORD}'
+# HOST = f'{pass_token.HOST}'
+# USER_MYSQL = f'{pass_token.USER_MYSQL}'
+# PASSWORD_MYSQL = f'{pass_token.PASSWORD_MYSQL}'
+# DATABASE_MYSQL = f'{pass_token.DATABASE_MYSQL}'
