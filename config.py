16a88b5f585d06f8c9e0458611fcc184f4c1e740
DmitriyPudlo <teinalandergor@gmail.com>
2023-01-24 00:58:17 +0500

    complete (for now)

--- config.py
+++ config.py
@@ -1,25 +1,24 @@
-# import pass_token
+import pass_token
 
-# TOKEN_WATER = f'{pass_token.TOKEN_WATER}'
-# TOKEN_GEO = f'{pass_token.TOKEN_GEO}'
-# TELEGRAM_TOKEN = f'{pass_token.TELEGRAM_TOKEN}'
-# DATABASE = 'postgres'
-# USER = 'postgres'
-# PASSWORD = f'{pass_token.PASSWORD}'
-# HOST = f'{pass_token.HOST}'
-# USER_MYSQL = f'{pass_token.USER_MYSQL}'
-# PASSWORD_MYSQL = f'{pass_token.PASSWORD_MYSQL}'
-# DATABASE_MYSQL = f'{pass_token.DATABASE_MYSQL}'
-
-TOKEN_WATER = input('Weather token: ')
-TOKEN_GEO = input('Geocode token: ')
-TELEGRAM_TOKEN = input('Telegram token: ')
-HOST = input('Host name: ')
-USER_MYSQL = input('User name: ')
-PASSWORD_MYSQL = input('Password MYSQL: ')
-DATABASE_MYSQL = input('Database name: ')
-PASSWORD = input('Password Postgres: ')
+TOKEN_WATER = f'{pass_token.TOKEN_WATER}'
+TOKEN_GEO = f'{pass_token.TOKEN_GEO}'
+TELEGRAM_TOKEN = f'{pass_token.TELEGRAM_TOKEN}'
 DATABASE = 'postgres'
 USER = 'postgres'
+PASSWORD = f'{pass_token.PASSWORD}'
+HOST = f'{pass_token.HOST}'
+USER_MYSQL = f'{pass_token.USER_MYSQL}'
+PASSWORD_MYSQL = f'{pass_token.PASSWORD_MYSQL}'
+DATABASE_MYSQL = f'{pass_token.DATABASE_MYSQL}'
 
+# TOKEN_WATER = input('Weather token: ')
+# TOKEN_GEO = input('Geocode token: ')
+# TELEGRAM_TOKEN = input('Telegram token: ')
+# HOST = input('Host name: ')
+# USER_MYSQL = input('User name: ')
+# PASSWORD_MYSQL = input('Password MYSQL: ')
+# DATABASE_MYSQL = input('Database name: ')
+# PASSWORD = input('Password Postgres: ')
+# DATABASE = 'postgres'
+# USER = 'postgres'
 
